5ecc3acd9e15961dab6e2ebd416ffc7463a5d732
sudiptobdcalling54-code <sudiptobdcalling54@gmail.com>
2025-10-06 17:56:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+//Section B 
+// 21. A Variable in Javascript is a container  used to store data values.
+let country = 'Bangladesh';
+console.log(country)//example
+
+
+//22.
+
+let student = {
+    name:'Maisha',
+    age: 22
+}
+console.log(student)
+
+//23
+
+let students = ['Maisha', 'Anika', 'Raida'];
+
+console.log(students[1])
+
+//Section C
+//26
+
+let x = 5;
+let y = 7;
+
+console.log(x==y, x===y)
+
+//27
+
+let arr = [1,2,3];
+arr.pop();
+arr.push(4);
+console.log(arr)
+
+//28 
+let obj = {
+    name:'John',
+    age: 25
+}
+
+console.log(obj.name)
+
+//29
+
+let numbers = [2,4,6,8];
+
+console.log(numbers[2])
+
+//30
+let text='Javascript';
+
+console.log(text.slice(4,10))
